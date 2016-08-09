39463b8f5685f30fdba9c80368ce1a78f78df320
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-09 11:29:31 -0400

    Mock response mappings added for account endpoints

---
+++ nessie-android-sdk/src/test/java/com/reimaginebanking/api/nessieandroidsdk/AccountTest.java
@@ -0,0 +1,27 @@
+package com.reimaginebanking.api.nessieandroidsdk;
+
+/**
+ * Tests all account endpoints against mock responses.
+ */
+public class AccountTest extends NessieTest {
+
+    /* GET /accounts */
+
+
+    /* GET /account/{id} */
+
+
+    /* GET /customer/{id}/accounts */
+
+
+    /* POST /customer/{id}/accounts */
+
+
+    /* PUT /accounts/{id} */
+
+
+    /* DELETE /accounts/{id} */
+
+    // success
+    // error
+}
