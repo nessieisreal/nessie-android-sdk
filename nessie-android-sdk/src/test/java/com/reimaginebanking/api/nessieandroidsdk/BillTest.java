4816716c821dd4ac4d6bb6c2bdff67bc79cf18e0
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-09 11:43:46 -0400

    Added stub test classes for all model objects

---
+++ nessie-android-sdk/src/test/java/com/reimaginebanking/api/nessieandroidsdk/BillTest.java
@@ -0,0 +1,20 @@
+package com.reimaginebanking.api.nessieandroidsdk;
+
+/**
+ * Tests all bill endpoints against mock responses.
+ */
+public class BillTest extends NessieTest {
+
+    /* GET /accounts/{id}/bills */
+
+    /* GET /bills/{id} */
+
+    /* GET /customers/{id}/bills */
+
+    /* POST /accounts/{id}/bills */
+
+    /* PUT /bills/{id} */
+
+    /* DELETE /bills/{id} */
+
+}
---
+++ nessie-android-sdk/src/test/java/com/reimaginebanking/api/nessieandroidsdk/BranchTest.java
@@ -0,0 +1,11 @@
+package com.reimaginebanking.api.nessieandroidsdk;
+
+/**
+ * Tests all branch endpoints against mock responses.
+ */
+public class BranchTest extends NessieTest {
+
+    /* GET /branches */
+
+    /* GET /branches/{id} */
+}
