9927fd7b7f7ad1ee215a5a8b6aa9d665da420ebb
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-14 00:35:55 -0400

    Merchant tests completed

--- nessie-android-sdk/src/test/java/com/reimaginebanking/api/nessieandroidsdk/MerchantTest.java
+++ nessie-android-sdk/src/test/java/com/reimaginebanking/api/nessieandroidsdk/MerchantTest.java
@@ -1,16 +1,94 @@
 package com.reimaginebanking.api.nessieandroidsdk;
 
+import com.reimaginebanking.api.nessieandroidsdk.models.Address;
+import com.reimaginebanking.api.nessieandroidsdk.models.Geocode;
+import com.reimaginebanking.api.nessieandroidsdk.models.Merchant;
+import com.reimaginebanking.api.nessieandroidsdk.models.PostResponse;
+import com.reimaginebanking.api.nessieandroidsdk.models.PutDeleteResponse;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Tests all merchant endpoints against mock responses.
  */
 public class MerchantTest extends NessieTest {
 
     /* GET /merchants */
+    @Test
+    public void testGetMerchants() throws Exception {
+        client.MERCHANT.getMerchants(new NessieTestResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                List<Merchant> merchants = (List<Merchant>) result;
+                assertEquals(4, merchants.size());
+                assertEquals("56c66be6a73e492741507624", merchants.get(0).getId());
+                assertEquals("Jetro", merchants.get(3).getName());
+            }
+        });
+    }
 
     /* GET /merchants/{id} */
+    @Test
+    public void testGetMerchant() throws Exception {
+        client.MERCHANT.getMerchant("123", new NessieTestResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                Merchant merchant = (Merchant) result;
+                assertEquals("Wells Fargo Center", merchant.getName());
+            }
+        });
+    }
 
     /* POST /merchants */
+    @Test
+    public void testCreateMerchant() throws Exception {
+        Address address = new Address.Builder()
+            .streetNumber("1111")
+            .streetName("Pittsburgh St")
+            .city("Pittsburgh")
+            .state("PA")
+            .zip("12345")
+            .build();
+
+        Geocode geocode = new Geocode(100, 100);
+
+        Merchant merchant = new Merchant.Builder()
+            .name("Grocery Store")
+            .category("Food")
+            .address(address)
+            .geocode(geocode)
+            .build();
+
+        client.MERCHANT.createMerchant(merchant, new NessieTestResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                PostResponse<Merchant> response = (PostResponse<Merchant>) result;
+                assertEquals(201, response.getCode());
+                assertEquals("Created merchant", response.getMessage());
+                assertEquals("Grocery Store", response.getObjectCreated().getName());
+            }
+        });
+    }
 
     /* PUT /merchants/{id} */
+    @Test
+    public void testUpdateMerchant() throws Exception {
+        Merchant merchant = new Merchant.Builder()
+            .name("Groceries")
+            .build();
+
+        client.MERCHANT.updateMerchant("123", merchant, new NessieTestResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                PutDeleteResponse response = (PutDeleteResponse) result;
+                assertEquals(202, response.getCode());
+                assertEquals("Accepted merchant modification", response.getMessage());
+            }
+        });
+    }
 
 }
