9bd640fe4bf0cb39a91edbfe12a34312e2bac61d
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-15 14:49:52 -0400

    Removed public from interface method declarations

--- nessie-android-sdk/src/main/java/com/reimaginebanking/api/nessieandroidsdk/NessieResultsListener.java
+++ nessie-android-sdk/src/main/java/com/reimaginebanking/api/nessieandroidsdk/NessieResultsListener.java
@@ -4,6 +4,6 @@ package com.reimaginebanking.api.nessieandroidsdk;
  * Custom results listener which defines callback methods for success and error on API response.
  */
 public interface NessieResultsListener {
-    public void onSuccess(Object result);
-    public void onFailure(NessieError error);
+    void onSuccess(Object result);
+    void onFailure(NessieError error);
 }
